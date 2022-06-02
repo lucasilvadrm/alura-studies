b8adfb83c2e2ca9c545a2973e2535a40ce0b2ad7
Lucas Silva <lucassilva.uern@gmail.com>
2022-06-02 11:42:24 -0300

    correções

--- src/components/Cronometro/Relogio/index.tsx
+++ src/components/Cronometro/Relogio/index.tsx
@@ -1,13 +1,13 @@
 import style from './Relogio.module.scss';
 
 export default function Relogio() {
-    return (
-        <>
-            <span className={style.relogioNumero}>0</span>
-            <span className={style.relogioNumero}>0</span>
-            <span className={style.relogioDivisao}>:</span>
-            <span className={style.relogioNumero}>0</span>
-            <span className={style.relogioNumero}>0</span>
-        </>
-    )
+  return (
+    <>
+      <span className={style.relogioNumero}>0</span>
+      <span className={style.relogioNumero}>0</span>
+      <span className={style.relogioDivisao}>:</span>
+      <span className={style.relogioNumero}>0</span>
+      <span className={style.relogioNumero}>0</span>
+    </>
+  )
 }
